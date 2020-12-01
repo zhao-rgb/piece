31680d1182c36b68450669b8b416972102cab7cf
zhao-rgb <1019916061@qq.com>
2020-12-01 10:47:30 +0800

    支付页面

---
+++ naixue-app/pages/remark/remark.vue
@@ -0,0 +1,64 @@
+<!-- 订单备注页面 -->
+<template>
+	<view class="container w-100 h-100 overflow-hidden">
+		<view class="textarea">
+			<textarea
+			placeholder-class="text-color-assist font-size-base"
+			v-model="remark"
+			class="{'text-color-danger' : remarkLength > 50,"></textarea>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+	methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+	padding: 30rpx 40rpx;
+
+	.textarea {
+		position: relative;
+
+		.remark {
+			border-radius: 8rpx;
+			padding: 30rpx 40rpx;
+			height: 320rpx;
+			color: $font-size-base;
+		}
+
+		.tips {
+			position: absolute;
+			bottom: 30rpx;
+			right: 40rpx;
+		}
+	}
+
+	.quick-inputs {
+		padding-right: 20rpx;
+
+		.quick-input {
+			background-color: #ffffff;
+			border: 2rpx solid $color-primary;
+			color: $color-primary;
+			font-size: $font-size-base;
+			padding: 16rpx 26rpx;
+			margin-right: 20rpx;
+			margin-bottom: 20rpx;
+		}
+	}
+
+	.submit-btn {
+		width: 90%;
+		height: 80rpx;
+		border-radius: 40rpx;
+		line-height: 80rpx;
+	}
+}
+</style>
